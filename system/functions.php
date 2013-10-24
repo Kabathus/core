71f98f716a17ab02c69775a28ff5fc7738571e9c
Tristan Lins <tristan.lins@bit3.de>
2013-10-24 16:15:29 +0200

    Add the missing user deprecated error code  (see #6105)

--- system/functions.php
+++ system/functions.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -133,7 +133,8 @@ function __error($intType, $strMessage, $strFile, $intLine)
 		E_USER_NOTICE       => 'Notice',
 		E_STRICT            => 'Runtime notice',
 		4096                => 'Recoverable error',
-		8192                => 'Deprecated notice'
+		8192                => 'Deprecated notice',
+		16384               => 'Deprecated notice'
 	);
 
 	// Ignore functions with an error control operator (@function_name)
